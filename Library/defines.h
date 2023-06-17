ed6d94ec06cde745e308e040a25fee876eb8658d
karamm77 <96154781+karamm77@users.noreply.github.com>
2023-06-17 22:29:03 +0300

    Add files via upload

---
+++ Library/defines.h
@@ -0,0 +1,17 @@
+#ifndef Defines_H_
+#define Defines_H_
+
+#define ENABLED			1u
+#define DISABLED		0u
+
+
+#define NULL		((void*)0)
+
+#define OK					0u
+#define NOK					1u
+#define NULL_PTR_ERR		2u
+
+
+
+
+#endif
