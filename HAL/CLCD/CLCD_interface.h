92cb8ebd605bc84d05f1fdba654733da3510c416
karamm77 <96154781+karamm77@users.noreply.github.com>
2023-06-17 22:20:18 +0300

    Add files via upload

---
+++ HAL/CLCD/CLCD_interface.h
@@ -0,0 +1,21 @@
+#ifndef CLCD_INTERFACE_H_
+#define CLCD_INTERFACE_H_
+
+void CLCD_voidSendCmd(uint8 Copy_u8Cmd);
+
+void CLCD_voidSendData(uint8 Copy_u8Data);
+
+uint8 CLCD_u8SendString(const char* Copy_chString);
+
+void CLCD_voidInit(void);
+
+void CLCD_voidSendNumber(sint32 Copy_s32Number);
+
+void CLCD_voidGetBinary(sint32 Copy_s32Decimal);
+
+void CLCD_voidGoToXY(uint8 Copy_u8XPos, uint8 Copy_u8YPos );
+
+void CLCD_voidSendSpecialCharacter(uint8 *Copy_pu8Pattern,uint8 Copy_u8PatternNum,uint8 Copy_u8XPos, uint8 Copy_u8YPos);
+
+void CLCD_voidMoveShape(uint8 Copy_u8XPos,uint8 Copy_u8YPos);
+#endif
