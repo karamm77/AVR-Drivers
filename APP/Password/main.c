d53715012450f2eab18508d5795f3933f8666180
karamm77 <96154781+karamm77@users.noreply.github.com>
2023-06-22 04:08:58 +0300

    Add files via upload

---
+++ APP/Password/main.c
@@ -0,0 +1,60 @@
+#include "BIT_MATH.h"
+#include "STD_TYPES.h"
+#include "DIO_interface.h"
+#include "CLCD_interface.h"
+#include "Keypad_interface.h"
+#include "util/delay.h"
+
+void main(void)
+{
+    CLCD_voidInit();
+    KEYPAD_voidInit();
+
+    CLCD_u8SendString("Pass:");
+    _delay_ms(2000);
+
+    CLCD_voidGoToXY(1, 0);
+
+    uint8 x[3] = {1, 1, 2};
+    uint8 pass[3];
+    uint8 i = 0, j = 0;
+    uint8 flag = 0;
+
+
+
+
+
+    while(1)
+    {
+    	  if(flag == 0 && j != 3)
+    	    {
+    	        for (i = 0; i < 3; i++) {
+    	            do {
+    	                pass[i] = KEYPAD_GetPressedKey();
+    	            } while (pass[i] == 0xFF);
+    	            CLCD_voidSendData('*');
+    	        }
+
+    	        if ((pass[0] == x[0]) && (pass[1] == x[1]) && (pass[2] == x[2])) {
+    	            flag = 1;
+    	            CLCD_voidClrScr();
+    	            CLCD_u8SendString("Welcome");
+
+    	        } else {
+    	            flag = 0;
+    	            CLCD_voidClrScr();
+    	            CLCD_u8SendString("Fail");
+    	            _delay_ms(1000);
+
+    	            CLCD_voidClrScr();
+    	            CLCD_u8SendString("Pass:");
+    	            _delay_ms(2000);
+
+    	            CLCD_voidGoToXY(1, 0);
+
+    	        }
+
+    	    }
+    	}
+    }
+
